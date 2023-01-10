9e29d894d0f01d72a99533336ed837203986355b
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-10 23:08:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import * as action from "./store/actions";
-// import { createStore } from "./store/createStore";
 import { initialStore } from "./store/store";
-// import { taskReducer } from "./store/taskReducer";
 
-// const initialState = [
-//   { id: 1, title: "Task 1", completed: false },
-//   { id: 2, title: "Task 2", completed: false },
-// ];
 
 const store = initialStore();
 
 function App() {
-  console.log(store.getState());
   const [state, setState] = useState(store.getState());
 
   useEffect(() => {
