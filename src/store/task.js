bf80f41b8fe57c3ad30d975554f0c1b019b4d7d2
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-11 16:52:47 +0300

    Redux Toolkit. Create Sliсe

--- src/store/task.js
+++ src/store/task.js
@@ -1,11 +1,31 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-const updated = createAction("task/updated");
-const remove = createAction("task/removed");
 const initialState = [
   { id: 1, title: "Task 1", completed: false },
   { id: 2, title: "Task 2", completed: false },
 ];
+
+const taskSlice = createSlice({
+  name: "task",
+  initialState,
+  reducers: {
+    updated(state, action) {
+      const elementIndex = state.findIndex(
+        (element) => element.id === action.payload.id
+      );
+      state[elementIndex] = {
+        ...state[elementIndex],
+        ...action.payload,
+      };
+    },
+    remove(state, action) {
+      return state.filter((element) => element.id !== action.payload.id);
+    },
+  },
+});
+
+const { actions, reducer: taskReducer } = taskSlice;
+const { updated, remove } = actions;
 
 export function taskCompleted(id) {
   return updated({ id, completed: true });
@@ -23,21 +43,5 @@ export function taskDelete(id) {
     id,
   });
 }
-
-const taskReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(updated, (state, action) => {
-      const elementIndex = state.findIndex(
-        (element) => element.id === action.payload.id
-      );
-      state[elementIndex] = {
-        ...state[elementIndex],
-        ...action.payload,
-      };
-    })
-    .addCase(remove, (state, action) => {
-      return state.filter((element) => element.id !== action.payload.id);
-    });
-});
 
 export default taskReducer;
