d86bab8c50c0977e2a627ed069751aa75b177cec
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-01-11 19:57:00 +0300

    Создаём Middleware. Логгер

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, compose, applyMiddleware } from "redux";
+import { logger } from "./middleware/logger";
 import taskReducer from "./task";
+
+const middlewareEnhancer = applyMiddleware(logger);
 
 function configureStore() {
   return createStore(
     taskReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      middlewareEnhancer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 }
 
